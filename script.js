ce30cf9ab422d185993d71ec3d8d39cc881c0a39
tosinalo <tosin_alo@ymail.com>
2022-08-31 21:56:56 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-console.log{"Hello"}
+console.log ("Hello")
 
 const h1tag = document.getElementById("text")
 let username = "Miracle"
 
-h1tag.textContent = "welcome, "
+h1Tag.textContent = "welcome, " + ask_user
+
+
